0776a6a73d07f943297351c746566c5a2bae1a8e
abduljawali05-wq <abduljawali05@gmail.com>
2026-05-04 10:07:51 +0530

    Update java1.java

--- java1.java
+++ java1.java
@@ -1,7 +1,7 @@
-public class java1 {
+ public class java1 {
     public static void main(String args[]){
-        System.out.println("hello java programming")
-        System.out.println("Bye!!")
+        System.out.println("hello java programming");
+        System.out.println("Bye!!");       
     }
 
 }
